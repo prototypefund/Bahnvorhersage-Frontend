e6b1e1f0b90dd5f8e0ff1b5ec5cdaebd6cc5cd45
McToel <theo.doellmann@gmx.de>
2023-05-11 16:49:47 +0200

    Added tooltips to show wich data source for minimal transfer time is used

--- src/components/NeededTransferDuration.vue
+++ src/components/NeededTransferDuration.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="min-transfer-duration m-3">
-    Minimale Umsteigezeit: {{ durationString }}{{ distanceString }}
+    Minimale Umsteigezeit:
+    <a
+      class="link-light"
+      data-bs-toggle="tooltip"
+      :data-bs-title="tooltipString"
+      ref="tooltip"
+      >{{ durationString }}</a
+    >
+    {{ distanceString }}
+    {{ platformString }}
   </div>
 </template>
 
 <script lang="ts">
 import { Duration } from "dayjs/plugin/duration";
 import { defineComponent } from "vue";
+import { Tooltip } from "bootstrap";
 
 interface TransferDuration {
   duration: Duration | null;
@@ -29,6 +39,9 @@ export default defineComponent({
       required: true,
     },
   },
+  mounted: function () {
+    new Tooltip(this.$refs.tooltip);
+  },
   computed: {
     durationString: function () {
       if (this.transfer.frequentTraveller.duration.seconds() == 0) {
@@ -46,6 +59,43 @@ export default defineComponent({
           Math.round(this.transfer.frequentTraveller.distance) +
           " m Fußweg"
         );
+      }
+    },
+    platformString: function () {
+      if (this.transfer.identicalPhysicalPlatform) {
+        return " (gleicher Bahnsteig)";
+      } else {
+        return "";
+      }
+    },
+    tooltipString: function () {
+      switch (this.transfer.source) {
+        case "RIL420":
+          return (
+            "Laut interner Richtlinie 420 der DB werden an diesem Bahnhof " +
+            this.durationString +
+            " Umsteigezeit angenommen. In der Regel ist diese Zeit ziemlich großzügig bemessen."
+          );
+        case "INDOOR_ROUTING":
+          return (
+            "Laut Wegberechnung werden an diesem Bahnhof " +
+            this.durationString +
+            " Umsteigezeit angenommen. In der Regel ist diese Zeit sehr knapp bemessen."
+          );
+        case "EFZ":
+          return (
+            "Laut Europäischem Fahrplanzentrum werden an diesem Bahnhof " +
+            this.durationString +
+            " Umsteigezeit angenommen. In der Regel ist diese Zeit ziemlich großzügig bemessen."
+          );
+        case "FALLBACK":
+          return (
+            "Für diesen Bahnhof sind uns leider keine Umsteigezeiten bekannt. Wir nehmen daher eine Mindestumsteigezeit von " +
+            this.durationString +
+            " an."
+          );
+        default:
+          return "bla";
       }
     },
   },
